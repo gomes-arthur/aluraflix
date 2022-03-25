9a98611e4de421d31ab573d8cb0c82a219fb8326
gomes-arthur <arthur.gomes.trabalho@gmail.com>
2022-03-25 16:47:17 -0300

    primeiro commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Funções no AluraFlix - Imersão Dev 2022 - Alura
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/gomes-arthur/pen/zYpYXgd](https://codepen.io/gomes-arthur/pen/zYpYXgd).
+
+
---
+++ dist/script.js
@@ -0,0 +1,12 @@
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML =
+      elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+  } else {
+    console.error("Endereço de filme inválido");
+  }
+  document.getElementById("filme").value = "";
+}
